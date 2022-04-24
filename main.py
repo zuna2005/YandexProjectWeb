b46249ce142f2a21ac343e52cb3323c9efca5bc9
zuna2005 <nasiba.zulunova@gmail.com>
2022-04-24 14:29:52 +0600

    added link to shop

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
+@app.route('/main')
 def news():
     return render_template('base.html', title="Главная")
+
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html', title="Магазин")
 
 
 if __name__ == '__main__':
